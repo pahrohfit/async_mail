ea706224cb307605e0d2df5b98a5c295816b08d2
Rob Dailey <rob@wargam.es>
2022-07-13 13:12:57 -0400

    Added basic app_factory

--- async_mail/__init__.py
+++ async_mail/__init__.py
@@ -1,3 +1,10 @@
 from async_mail.backends import get_backend
 
-Mail = get_backend()
+#Mail = get_backend()
+Mail = None
+
+def init_app(app = None):
+    if app:
+        Mail = get_backend()
+
+    return Mail

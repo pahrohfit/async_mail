658da4f32f62fae9c976f22f7ae84e450901f1a0
Rob Dailey <rob@wargam.es>
2022-07-13 13:26:12 -0400

    Factory work

--- async_mail/__init__.py
+++ async_mail/__init__.py
@@ -3,8 +3,15 @@ from async_mail.backends import get_backend
 #Mail = get_backend()
 Mail: object = ()
 
-def init_app(app = None):
-    if app:
-        Mail = get_backend()
+class Mail:
 
-    return Mail
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+
+    def init_app(self, app = None):
+        _backend = get_backend()
+        self = _backend
+
+        return _backend
